97b77a155b59c5177ec5e5a3730bf157e3d8485f
gilvaju <gcostajr@gmail.com>
2017-03-27 12:19:58 -0300

    corrigindo linebreaks e adicionando componentes para sidebar e navbar

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+      <!-- Sidebar navigation -->
+      <sidebar-compoenent></sidebar-compoenent>
+      <!--/. Sidebar navigation -->
+
+      <!--Navbar-->
+      <navbar-component></navbar-component>
+      <!--/.Navbar-->
+  </header>
+</template>
+
+<script>
+import SidebarComponent from '@/components/layout/Sidebar'
+import NavbarComponent from '@/components/layout/Navbar'
+
+export default {
+  components: {
+    SidebarComponent, NavbarComponent,
+  },
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!--Double navigation-->
+    <header-component></header-component>
+    <!--/Double navigation-->
+
+    <!--Main layout-->
+    <main>
+      <div class="container-fluid">
+        <div>
+          <h1>Welcome to Your Vue.js App</h1>
+          <h2>Essential Links</h2>
+        </div>
+      </div>
+    </main>
+    <!--/Main layout-->
+  </div>
+</template>
+
+<script>
+import HeaderComponent from '@/components/Header'
+
+export default {
+  components: {
+    HeaderComponent,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
